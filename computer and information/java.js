abffbbcc8c15c2c79041de63895498bf850da27a
amirazak11 <amirazak1032000@gmail.com>
2021-02-07 13:33:47 +0200

    added login and chat

---
+++ computer and information/java.js
@@ -0,0 +1,11 @@
+function openNav(){
+    console.log("open")
+  document.getElementById("mySidenav").style.width="17%";
+  document.getElementById("main").style.marginLeft="17%";
+  
+}
+function closeNav(){
+    console.log("close")
+  document.getElementById("mySidenav").style.width="0%";
+  document.getElementById("main").style.marginLeft="0%";
+}
